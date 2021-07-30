fc5991360c193bfd7bdfa1275ad6633d77e5db68
Huihui <xingjf3@mail2.sysu.edu.cn>
2021-07-31 02:29:17 +0800

    Add distributed training procedure

---
+++ run-classification-distributed.py
@@ -0,0 +1,65 @@
+
+import os
+
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+import torch
+
+from datasets.Imagenet import ImageNet
+from datasets.Transforms import common_test, common_train
+from models.resnet import resnet18
+from runners.classification import DistRunner
+
+def get_device_id():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, default=0)
+    args = parser.parse_args()
+    return args.local_rank
+
+def main():
+    torch.distributed.init_process_group(
+        backend='nccl',
+        # init_method=f'file://{os.environ["TMPDIR"]}/Simclr'
+        init_method='env://'
+    )
+    device_id = get_device_id()
+    torch.cuda.set_device(device_id)
+    device = f'cuda:{device_id}'
+    scaler = torch.cuda.amp.GradScaler()
+
+    batch_size = 256
+    normalize = {
+        'mean': [0.485, 0.456, 0.406],
+        'std': [0.229, 0.224, 0.225]
+    }
+    lr = 1e-1
+    epochs = 200
+
+    # transform = common_train((224, 224))
+    trainset = ImageNet(os.environ['DATAROOT'], transform=common_train((224, 224)), train=True, subset=50)
+    testset = ImageNet(os.environ['DATAROOT'], transform=common_test((224, 224)), train=False, subset=50)
+    trainsampler = torch.utils.data.distributed.DistributedSampler(trainset)
+    testsampler = torch.utils.data.distributed.DistributedSampler(testset)
+    train_loader = DataLoader(trainset, batch_size=batch_size, sampler=trainsampler, pin_memory=False, num_workers=12)
+    test_loader = DataLoader(testset, batch_size=batch_size, sampler=testsampler, pin_memory=False, num_workers=12)
+
+
+    model = resnet18(**normalize, class_num=50).to(device)
+    model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[device_id], output_device=device_id)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4, nesterov=True)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs, 1e-4, -1)
+    # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 90], gamma=0.1, last_epoch=-1)
+
+    runner = DistRunner(model, train_loader, test_loader, criterion, optimizer, scheduler, scaler, epochs, 10)
+    
+    tqdm.write("Start training with Resnet18.")
+    runner.train()
+
+if __name__ == '__main__':
+    # python -m torch.distributed.launch --nproc_per_node=2 run-classification-distributed.py
+    main()
--- runners/classification.py
+++ runners/classification.py
@@ -79,3 +79,104 @@ class Runner():
                 tqdm.write("Evaluation {}/{}, Loss avg. {:.4f}, Acc. {:.4f}".format(epoch_idx, self.epochs, avg_loss, avg_acc))
 
         tqdm.write("Finish training!")
+
+
+class DistRunner():
+    def __init__(self, model, train_loader, test_loader, criterion, optimizer, scheduler=None, \
+                 scaler=None, epochs=120, eval_interval=10):
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.scaler = scaler
+        self.epochs = epochs
+        self.eval_interval = eval_interval
+        self.desc = lambda status, progress: f"{status}: {progress}"
+
+        self.device = next(self.model.parameters()).device
+
+    def collect(self, x, mode='mean'):
+        xt = torch.tensor([x]).to(self.device)
+        torch.distributed.all_reduce(xt, op=torch.distributed.ReduceOp.SUM)
+        # print(xt.item())
+        xt = xt.item()
+        if mode == 'mean':
+            xt /= torch.distributed.get_world_size()
+        return xt
+
+    def train_step(self, progress):
+        self.model.train()
+        loss_meter = AverageMeter()
+        pbar = tqdm(total=len(self.train_loader), leave=False, desc=self.desc("Train", progress))
+        for batch_idx, batch in enumerate(self.train_loader):
+            data, target = batch
+            data, target = data.to(self.device), target.to(self.device)
+            with torch.cuda.amp.autocast():
+                output = self.model(data)
+                # loss = nn.functional.cross_entropy(output, target)
+                loss = self.criterion(output, target)
+            pbar.set_postfix_str("Loss {:.4f}".format(loss.item()))
+            loss_meter.update(loss.item())
+
+            self.optimizer.zero_grad()
+            if self.scaler is not None:
+                self.scaler.scale(loss).backward()
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+            else:
+                loss.backward()
+                self.optimizer.step()
+
+            pbar.update(1)
+        pbar.close()
+
+        return loss_meter.report()
+
+    def evaluate(self, progress):
+        self.model.eval()
+        accuracy_meter = AverageMeter()
+        loss_meter = AverageMeter()
+        with torch.no_grad():
+            pbar = tqdm(total=len(self.test_loader), leave=False, desc=self.desc("Eval", progress))
+            for batch_idx, (data, target) in enumerate(self.test_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                loss = self.criterion(output, target)
+                loss_meter.update(loss.item())
+                pred = output.argmax(dim=1)
+
+                true_positive = (pred == target).sum().item()
+                total = pred.shape[0]
+                accuracy_meter.update(true_positive, total)
+                
+                pbar.update(1)
+            pbar.close()
+        
+        return (loss_meter.report(), accuracy_meter.sum, accuracy_meter.count)
+
+    def train(self):
+        (avg_loss, acc_sum, acc_count) = self.evaluate("Init")
+        avg_loss = self.collect(avg_loss)
+        avg_acc = self.collect(acc_sum, mode='sum') / self.collect(acc_count, mode='sum')
+        if torch.distributed.get_rank() == 0:
+            tqdm.write("Evaluation init, Loss avg. {:.4f}, Acc. {:.4f}".format(avg_loss, avg_acc))
+
+        for epoch_idx in range(self.epochs):
+            avg_loss = self.train_step("{}/{}".format(epoch_idx, self.epochs))
+            avg_loss = self.collect(avg_loss)
+            if torch.distributed.get_rank() == 0:
+                tqdm.write("Training procedure {} (total {}), Loss avg. {:.4f}".format(epoch_idx, self.epochs, avg_loss))
+            
+            if self.scheduler is not None:
+                self.scheduler.step()
+
+            if epoch_idx % self.eval_interval == (self.eval_interval-1):
+                avg_loss, acc_sum, acc_count = self.evaluate("{}/{}".format(epoch_idx, self.epochs))
+                avg_loss = self.collect(avg_loss)
+                avg_acc = self.collect(acc_sum, mode='sum') / self.collect(acc_count, mode='sum')
+                if torch.distributed.get_rank() == 0:
+                    tqdm.write("Evaluation {}/{}, Loss avg. {:.4f}, Acc. {:.4f}".format(epoch_idx, self.epochs, avg_loss, avg_acc))
+
+        tqdm.write("Finish training!")
